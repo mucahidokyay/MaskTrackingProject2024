71ca0b9faaa6156083375671d9123660aeed2e02
mucahidokyay <mucahidokyay@outlook.com>
2024-02-10 23:44:05 +0300

    MaskTrackingProject2024
    c# dilini kullanarak maske dağıtım takip projesi yaptım. daha geliştirme aşamasında geliştirmeleri yaptıkça güncelleme yapacağım

---
+++ Workaround/Program.cs
@@ -0,0 +1,13 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "mücahid";
+person1.LastName = "okyay";
+person1.DateOfBirthYear = 2000;
+person1.NationalIdentity = 12345678910;
+
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
